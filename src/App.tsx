81bb5acc34a238e48eac778ce567b8449c304014
ChrisNieves <nievescs20@gmail.com>
2023-01-21 02:54:22 -0500

    configurations completed

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="text-secondary-300">Hello World</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-100": "#3E92CC",
+        "primary-300": "#2A628F",
+        "primary-500": "#13293D",
+        "secondary-300": "#6D7275",
+        "secondary-500": "#40434E",
+      },
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+};
